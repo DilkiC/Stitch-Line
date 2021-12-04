1d57e1ebf68b71f54179643e342aec6c0944542a
DilkiC <dilkicooray97@gmail.com>
2021-12-04 23:16:59 +0530

    frontend_initial

---
+++ frontend/stitchline/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/auth/login', pathMatch: 'full'},
+
+  {path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
+  {path: 'default', loadChildren: () => import('./modules/default/default.module').then(m => m.DefaultModule) }
+  ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
