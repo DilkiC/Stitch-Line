79696650cc0c54b41d77dcafc1b8a031904afd33
DilkiC <dilkicooray97@gmail.com>
2021-12-05 15:58:14 +0530

    login_ui

--- frontend/stitchline/src/app/app-routing.module.ts
+++ frontend/stitchline/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./components/login/login.component";
+import {RegisterComponent} from "./components/register/register.component";
+import {HomeComponent} from "./components/home/home.component";
 
 const routes: Routes = [
-  {path: '', redirectTo: '/auth/login', pathMatch: 'full'},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'home',component:HomeComponent},
+  {path:'',redirectTo:'login',pathMatch:'full'}
 
-  {path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
-  {path: 'default', loadChildren: () => import('./modules/default/default.module').then(m => m.DefaultModule) }
   ];
 
 @NgModule({
---
+++ frontend/stitchline/src/app/components/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  login:any = FormGroup;
+  constructor(private fb:FormBuilder ,private router:Router) { }
+
+  ngOnInit(): void {
+    this.login = this.fb.group({
+      name:['',Validators.required],
+      password:['',Validators.required]
+    })
+  }
+
+  loginSubmit(data:any){
+    console.log(data);
+  }
+
+  goToSignup(){
+    this.router.navigate(["register"]);
+
+  }
+
+}
